7ac3be27b6ec6cf634cba50d53be7667d09c6677
Ronald Gatewood <ronald.l.gatewood@gmail.com>
2014-05-23 11:17:48 -0400

    pset1 now runs

--- pset1/mario.java
+++ pset1/mario.java
@@ -2,33 +2,23 @@ import java.io.Console;
 
 public class mario {
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) {
     int height;
     
-    do
-    {
-      System.out.println("Height: ");
-      height = parseInt(System.console().readLine("Height: "));
-      
-    
+    do{
+      height = Integer.parseInt(System.console().readLine("Height: "));  
     } while ((height<0) || (height>23));
     
-    for( int y = 0; y < height; y++) 
-    {
-        for ( int x = height ; x > 0; x--)
-        {   
-            if ((x-1)>y)
-            { 
-                System.out.println(" ");
-            }
-            else
-            {
-                System.out.println("#");
-            }
+    for( int y = 0; y < height; y++) {
+      for ( int x = height ; x > 0; x--) {   
+        if ((x-1)>y) { 
+          System.out.print(" ");
         }
-        System.out.println("#\n");   
-    } 
+        else {
+          System.out.print("#");
+        }
+      }
+      System.out.print("#\n");   
+    }
   }
-
 }
